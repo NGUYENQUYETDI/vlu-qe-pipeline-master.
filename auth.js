331c7837ba42264c55fdb4fe1c408efeff90ed14
NGUYENQUYETDI <165255394+NGUYENQUYETDI@users.noreply.github.com>
2026-09-21 13:53:31 +0700

    Add login function with user validation
    Implement login function with validation checks for username and password.

---
+++ auth.js
@@ -0,0 +1,22 @@
+function login(username, password) {
+  if (!username || !password) {
+    throw new Error('Username và password không được để trống');
+  }
+
+  if (username === 'locked_user') {
+    throw new Error('Tài khoản đã bị khóa');
+  }
+
+  const specialCharRegex = /[!@#$%^&*(),.?":{}|<>]/;
+  if (specialCharRegex.test(password)) {
+    throw new Error('Mật khẩu không được chứa ký tự đặc biệt');
+  }
+
+  if (username === 'admin' && password === '123') {
+    return true;
+  }
+
+  return false;
+}
+
+module.exports = { login };
